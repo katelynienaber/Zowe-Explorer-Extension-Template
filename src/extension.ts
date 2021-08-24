77e5a79bc266303af81670867375f67754f0b5b7
Katelyn Nienaber <katelyn.nienaber@broadcom.com>
2021-08-24 15:43:17 +0200

    Example of the Zowe Explorer API logger
    Signed-off-by: Katelyn Nienaber <katelyn.nienaber@broadcom.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
+import { IZoweLogger, ZoweVsCodeExtension } from '@zowe/zowe-explorer-api';
+import path = require('path');
 import * as vscode from 'vscode';
 import { getProfileInfo, setConfiguration } from './utilities';
-
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand("ze-ext.setConfiguration", () => setConfiguration()));
@@ -13,4 +14,15 @@ export function activate(context: vscode.ExtensionContext) {
         }
     }));
 
+    const logLocation = path.join(__dirname, "/logs/ZE");
+    const loggerMessage = "Test error!";
+
+    // Initialize the Zowe Explorer logger
+    const logger = new IZoweLogger("Zowe Explorer Extension Template", logLocation);
+
+    // Log an error to the log file only
+    logger.logImperativeMessage(loggerMessage, 4);
+
+    // Show an error to the user in VSCode, and log it to the log file
+    ZoweVsCodeExtension.showVsCodeMessage(loggerMessage, 4, logger);
 }
